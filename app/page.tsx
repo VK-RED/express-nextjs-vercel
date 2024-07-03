6976d749fbd6fb05b11571baae8621b7a832cee3
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-03 21:05:27 +0530

    test backend route

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+  async function getData(){
+    const res = await fetch("https://express-vk.vercel.app/");
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
